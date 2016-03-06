dc37e4de64e56e1ba53eb3976d2ec4c77bd9eaf2
Juliana Davila <judaro13@gmail.com>
2016-03-06 15:43:09 -0500

    Created tree herarchy for the ebay categories, uploaded to the SQLite DB and rendering

--- challenge.go
+++ challenge.go
@@ -4,103 +4,155 @@ import (
 	"database/sql"
 	"encoding/xml"
 	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3" // go get github.com/mattn/go-sqlite3
 	flag "github.com/ogier/pflag"   //go get github.com/ogier/pflag
 )
 
 var (
-	xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
-<GetCategoriesResponse xmlns="urn:ebay:apis:eBLBaseComponents">
-  <Timestamp>2016-03-05T19:19:08.199Z</Timestamp>
-  <Ack>Success</Ack>
-  <Version>949</Version>
-  <Build>E949_CORE_API_17785418_R1</Build>
-  <CategoryArray>
-    <Category>
-      <BestOfferEnabled>true</BestOfferEnabled>
-      <CategoryID>10542</CategoryID>
-      <CategoryLevel>1</CategoryLevel>
-      <CategoryName>Real Estate</CategoryName>
-      <CategoryParentID>10542</CategoryParentID>
-      <LSD>true</LSD>
-    </Category>
-    <Category>
-      <BestOfferEnabled>true</BestOfferEnabled>
-      <CategoryID>15825</CategoryID>
-      <CategoryLevel>2</CategoryLevel>
-      <CategoryName>Commercial</CategoryName>
-      <CategoryParentID>10542</CategoryParentID>
-      <LeafCategory>true</LeafCategory>
-      <LSD>true</LSD>
-    </Category>
-    <Category>
-      <BestOfferEnabled>true</BestOfferEnabled>
-      <CategoryID>1607</CategoryID>
-      <CategoryLevel>2</CategoryLevel>
-      <CategoryName>Other Real Estate</CategoryName>
-      <CategoryParentID>10542</CategoryParentID>
-      <LeafCategory>true</LeafCategory>
-      <LSD>true</LSD>
-    </Category>
-  </CategoryArray>
-  <CategoryCount>7</CategoryCount>
-  <UpdateTime>2015-09-01T22:57:09.000Z</UpdateTime>
-  <CategoryVersion>113</CategoryVersion>
-  <ReservePriceAllowed>true</ReservePriceAllowed>
-  <MinimumReservePrice>0.0</MinimumReservePrice>
-</GetCategoriesResponse>
-`
+	xmlContent = ``
 )
 
+// SQL constants
 const (
-	insertCategorySQL = `insert into category(id, bestOfferEnabled, level, parentId, name, LeafCategory, lsd ) values(?, ?, ?, ?, ?, ?, ?)`
+	createCategoriesTable = `create table categories (
+        id integer not null primary key, 
+        bestOfferEnabled boolean,
+        level integer,
+        parentId integer,
+        name text,
+        leafCategory boolean,
+        lsd boolean,
+        eldersPath text, 
+        FOREIGN KEY(parentId) REFERENCES category(id)
+    );`
+	insertCategorySQL = `insert into categories(id, bestOfferEnabled, level, parentId, name, leafCategory, lsd, eldersPath) values(?, ?, ?, ?, ?, ?, ?, ?)`
+	queryCategory     = `SELECT 
+     id, bestOfferEnabled, level , parentId, name, leafCategory, lsd 
+     FROM categories WHERE id=? OR eldersPath LIKE ?
+     ORDER BY id ASC`
 )
 
 // GetCategoriesResponse contains the categories from the ebay API.
 type GetCategoriesResponse struct {
-	XMLName       xml.Name    `xml:"GetCategoriesResponse"`
+	XMLName xml.Name `xml:"GetCategoriesResponse"`
+
 	Timestamp     string      `xml:"Timestamp"`
+	Build         string      `xml:"Build"`
 	CategoryArray []*Category `xml:"CategoryArray>Category"`
 }
 
 // Category contains the categories
 type Category struct {
-	XMLName          xml.Name `xml:"Category"`
-	BestOfferEnabled bool     `xml:"BestOfferEnabled"`
-	ID               int      `xml:"CategoryID"`
-	Level            int      `xml:"CategoryLevel"`
-	Name             string   `xml:"CategoryName"`
-	ParentID         int      `xml:"CategoryParentID"`
-	LeafCategory     bool     `xml:"LeafCategory"`
-	LSD              bool     `xml:"LSD"`
+	XMLName          xml.Name    `xml:"Category"`
+	BestOfferEnabled bool        `xml:"BestOfferEnabled"`
+	ID               int         `xml:"CategoryID"`
+	Level            int         `xml:"CategoryLevel"`
+	Name             string      `xml:"CategoryName"`
+	ParentID         int         `xml:"CategoryParentID"`
+	LeafCategory     bool        `xml:"LeafCategory"`
+	LSD              bool        `xml:"LSD"`
+	Indexed          bool        `xml:"-"`
+	Children         []*Category `xml:"-"`
+	Parent           *Category   `xml:"-"`
 }
 
 func checkErr(err error) {
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
 func main() {
 	rebuildOpt := flag.Bool("rebuild", false, "Recreate the category DB tree")
-	renderOpt := flag.Int("render", 0, "Render a category with the given ID")
+	renderOpt := flag.Int("render", 0, "Render a category with the given ID. User as --render=12345 ")
 	flag.Parse()
 
 	if *rebuildOpt {
 		rebuild()
 	} else if *renderOpt > 0 {
-
+		render(*renderOpt)
 	} else {
 		flag.PrintDefaults()
 	}
 }
 
+func render(id int) {
+	dat, err := ioutil.ReadFile("category.html")
+	checkErr(err)
+
+	dict := getCategory(id)
+
+	t := template.Must(template.New("categories").Parse(string(dat)))
+	err = t.Execute(os.Stdout, dict)
+	if err != nil {
+		fmt.Println("executing template:", err)
+	}
+}
+
+func (c *Category) index(categories *GetCategoriesResponse) {
+	if c.Indexed {
+		return
+	}
+
+	parent := findParent(c.ParentID, categories)
+	if parent != nil && c.ID != c.ParentID {
+		c.Parent = parent
+		parent.Children = append(parent.Children, c)
+		parent.index(categories)
+	}
+
+	c.Indexed = true
+
+}
+
+func findParent(parentID int, categories *GetCategoriesResponse) *Category {
+	for _, category := range categories.CategoryArray {
+		if category.ID == parentID {
+			return category
+		}
+	}
+	return nil
+}
+
+func (c *Category) debug() {
+	fmt.Printf("%s%s\n", strings.Repeat("    ", c.Level), c.Name)
+	for _, child := range c.Children {
+		child.debug()
+	}
+}
+
+func (c *Category) findAncestors() string {
+	ancestors := "/"
+	category := c
+
+	for category != nil {
+		if category.Parent != nil {
+			ancestors = "/" + strconv.Itoa(category.ParentID) + ancestors
+			category = category.Parent
+		}
+	}
+	return ancestors
+}
+
 func rebuild() {
 	rebuildDB()
 	dict := &GetCategoriesResponse{}
-	xml.Unmarshal([]byte(xmlContent), dict)
+	content, err := ioutil.ReadFile("full")
+
+	xml.Unmarshal([]byte(content), dict)
+	// xml.Unmarshal([]byte(xmlContent), dict)
+
+	for _, category := range dict.CategoryArray {
+		category.index(dict)
+		fmt.Printf(".")
+	}
 
 	db, err := sql.Open("sqlite3", "challenge.db")
 	defer db.Close()
@@ -108,7 +160,6 @@ func rebuild() {
 
 	for _, category := range dict.CategoryArray {
 		insertCategory(db, category)
-		fmt.Printf("%#v\n", category)
 	}
 
 }
@@ -119,19 +170,7 @@ func rebuildDB() {
 	defer db.Close()
 	checkErr(err)
 
-	sqlStmt := `
-	create table category (
-        id integer not null primary key, 
-        bestOfferEnabled boolean,
-        level integer,
-        parentId integer,
-        name text,
-        LeafCategory boolean,
-        lsd boolean, 
-        FOREIGN KEY(parentId) REFERENCES category(idartistid)
-    );
-	`
-	_, err = db.Exec(sqlStmt)
+	_, err = db.Exec(createCategoriesTable)
 	checkErr(err)
 }
 
@@ -149,8 +188,43 @@ func insertCategory(db *sql.DB, category *Category) {
 		category.ParentID,
 		category.Name,
 		category.LeafCategory,
-		category.LSD)
+		category.LSD,
+		category.findAncestors())
 	checkErr(err)
 	tx.Commit()
 
+	fmt.Printf("*")
+}
+
+func getCategory(id int) *GetCategoriesResponse {
+	db, err := sql.Open("sqlite3", "challenge.db")
+	checkErr(err)
+
+	defer db.Close()
+	rows, err := db.Query(queryCategory, id, fmt.Sprintf(`%%/%d/%%`, id))
+	checkErr(err)
+
+	defer rows.Close()
+
+	dict := &GetCategoriesResponse{
+		CategoryArray: []*Category{},
+	}
+
+	for rows.Next() {
+		category := &Category{}
+		err = rows.Scan(&category.ID, &category.BestOfferEnabled, &category.Level, &category.ParentID, &category.Name, &category.LeafCategory, &category.LSD)
+		checkErr(err)
+		if category.ID >= id {
+			dict.CategoryArray = append(dict.CategoryArray, category)
+		}
+	}
+
+	// fmt.Printf("%v", catArray)
+
+	for _, category := range dict.CategoryArray {
+		category.index(dict)
+		fmt.Printf(".")
+	}
+	fmt.Println("")
+	return dict
 }
